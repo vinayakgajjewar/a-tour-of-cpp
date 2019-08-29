fb62d4ad087f404979b229be892f4ed091f55ea0
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2019-08-29 09:43:20 -0700

    Tests and loops

--- ch2.cpp
+++ ch2.cpp
@@ -14,6 +14,55 @@ void print_square(double x) {
     std::cout << "The square of " << x << " is " << square(x) << "\n";
 }
 
+bool accept() {
+    std::cout << "Do you want to accept? (y / n)\n";
+    char answer = 0;
+    std::cin >> answer;
+
+    if (answer == 'y') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool accept_2() {
+    std::cout << "Do you want to accept? (y / n)\n";
+    char answer = 0;
+    std::cin >> answer;
+
+    switch (answer) {
+        case 'y':
+            return true;
+        case 'n':
+            return false;
+        default: // If there is no default, then no action is taken if the input doesn't match any of the cases
+            std::cout << "I'll take that as a no.\n";
+            return false;
+    }
+}
+
+bool accept_3() {
+    int tries = 0;
+    while (tries < 3) {
+        std::cout << "Do you want to accept? (y / n)\n";
+        char answer = 0;
+        std::cin >> answer;
+
+        switch (answer) {
+            case 'y':
+                return true;
+            case 'n':
+                return false;
+            default:
+                std::cout << "I don't understand that.\n";
+                tries++;
+        }
+    }
+    std::cout << "I'll take that as a no.\n";
+    return false;
+}
+
 int main() {
 
     int inches = 13;
@@ -38,4 +87,8 @@ int main() {
 
     const int blah = 17; // A named constant
     constexpr double blah2 = summation(1.3, 2.4); // summation() is a constexpr so this is okay
+
+    std::cout << accept() << "\n";
+    std::cout << accept_2() << "\n";
+    std::cout << accept_3() << "\n";
 }
