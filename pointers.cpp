2f5f452772bc935e724447cd065f0a99f1c0ce9d
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2019-09-22 15:01:44 -0700

    Work on pointers

--- pointers.cpp
+++ pointers.cpp
@@ -21,6 +21,16 @@ int main() {
     int *array_pointer;
     // After this line, my_array and array_pointer would be very similar
     // However, array_pointer can be assigned a different address while my_array always points to the same block of 20 elements of type int
-    array_pointer = my_array; // This is valid
+    array_pointer = my_array; // This is valid because an array can always be implicitly converted to a pointer of the proper type
     //my_array = array_pointer; // Not valid
+
+    *(my_array+1) = 2; // Set the second index of my_array to 2 using the dereference operator
+    my_array[0] = 1; // Set the first index of my_array to 1 using the offset operator (the square brackets)
+
+    std::cout << my_array[0] << "\n";
+    std::cout << my_array[1] << "\n";
+
+    std::cout << *array_pointer << "\n";
+    *array_pointer = 3;
+    std::cout << *my_array << "\n";
 }
