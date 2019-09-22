2ad0335c56854f1615490c7ab1abf42cc2e35a92
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2019-09-22 15:49:19 -0700

    Add more comments to Classes 1

--- classes_1.cpp
+++ classes_1.cpp
@@ -4,8 +4,8 @@ class Rectangle {
     int width; // By default, width is private
     int height; // By default, height is private
 public:
-    void set_vals(int, int);
-    int get_area() {
+    void set_vals(int, int); // set_values has only been declared but its definition is outside of the class
+    int get_area() { // get_area's definition was included within the class definition because of its simplicity
         return width * height;
     }
 } rect; // Declare an object of type Rectangle
@@ -19,7 +19,7 @@ int main() {
 
     rect.set_vals(3, 4);
     std::cout << rect.get_area() << "\n";
-    Rectangle rect2;
+    Rectangle rect2; // Because a class is a type, we can have multiple objects of type Rectangle
     rect2.set_vals(5, 6);
     std::cout << rect2.get_area() << "\n";
 }
