b4e3cfaee77d10929e7460bfbccbd2e54d1bfa44
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2019-09-25 09:47:50 -0700

    Add initializer list example and reformat curly braces

--- data-structures.cpp
+++ data-structures.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 #include <string>
 
-struct vegetable {
+struct vegetable
+{
     std::string name;
     std::string color;
 } veggie_1;
 
-int main() {
-    vegetable veggie_2;
-
+int main()
+{
     veggie_1.name = "broccoli";
     veggie_1.color = "green";
-    veggie_2.name = "carrot";
-    veggie_2.color = "orange";
+
+    vegetable veggie_2 = {"carrot", "orange"}; // Using an initializer list to initialize a struct
 
     std::cout << veggie_1.name << "\n";
     std::cout << veggie_2.name << "\n";
